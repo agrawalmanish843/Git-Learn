fd807b9ec01a906a7712c44d814b072b4acf0e15
Manish Agrawal <agrawal.manish843@gmail.com>
2026-07-16 15:37:47 +0530

    Update Demo.java

--- src/Learn/Demo.java
+++ src/Learn/Demo.java
@@ -2,8 +2,8 @@ package Learn;
 
 public class Demo {
 public static void main(String[] args) {
-	System.out.println("Github EDIIT");
-	System.out.println("mani");
+	System.out.println("Github");
+	System.out.println("manish ");
 	
 }
 }
