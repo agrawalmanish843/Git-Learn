261d2cc6ccbafeef5659181bd87f668f62d3c497
Manish Agrawal <agrawal.manish843@gmail.com>
2026-07-16 15:30:26 +0530

    Update output message in Demo.java

--- src/Learn/Demo.java
+++ src/Learn/Demo.java
@@ -2,7 +2,7 @@ package Learn;
 
 public class Demo {
 public static void main(String[] args) {
-	System.out.println("Hello Git");
+	System.out.println("Github EDIIT");
 	System.out.println("mani");
 	
 }
