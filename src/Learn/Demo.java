06fae8021622098e403d13e228fd3cc9aa20304e
Manish@CDGI <agrawal.manish843@gmail.com>
2026-07-16 17:48:14 +0530

    Update Main Branch

--- src/Learn/Demo.java
+++ src/Learn/Demo.java
@@ -3,7 +3,6 @@ package Learn;
 public class Demo {
 public static void main(String[] args) {
 	System.out.println("Github");
-	System.out.println("manish ");
-	
+	System.out.println("Main Branch");
 }
 }
